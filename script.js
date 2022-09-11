851fbadecefb2cda3a014d769ebe30c3bab71a8e
khushi <khushishukla5890@gmail.com>
2022-09-11 16:50:47 +0530

    card added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const prev = 0;
 const info = document.querySelector('.pokemon-info')
 const btn_next = document.querySelector('.next-slide')
+const b1 = document.querySelector('.block1');
+const b2 = document.querySelector('.block2');
+const b3 = document.querySelector('.block3')
+const block = document.querySelectorAll('block3')
 // =========================== one method by using fetch ===========================
 
 // fetch(
@@ -14,9 +18,18 @@ const btn_next = document.querySelector('.next-slide')
 // .then(result => console.log(result))
 
 // ======================= with the help of npm package ============================
-const interval = {
-  offset: 0,
-  limit: 3,
+// const interval = {
+//   offset: 0,
+//   limit: 3,
+// }
+
+let index  = 0;
+const pokemon_data =[]
+const callingApi = async() =>{
+ const response = await P.getPokemonsList()
+ const data = await response.results;
+ pokemon_data.push(...data);
+ console.log(pokemon_data);
 }
 
 const image_value = async(url,div) =>{
@@ -35,76 +48,80 @@ const image_value = async(url,div) =>{
        div.append(img)
 }
 
-const pokemon_data =[]
-const callingApi = async() =>{
- const response = await P.getPokemonsList(interval)
- const data = await response.results;
- pokemon_data.push(...data);
- console.log(pokemon_data);
+
+callingApi();
+
+const addDiv = (idx) => {
+       const div  = document.createElement('div');
+       const h4 =  document.createElement('h4')
+       div.classList.add('card')
+       div.dataset.idx = idx;
+       image_value(pokemon_data[idx].url,div)
+    //    const para = document.createElement('p')
+       h4.textContent = pokemon_data[idx].name
+       
+       div.append(h4)
+       info.append(div)
 }
 
-let index  = 0;
+
 btn_next.addEventListener('click',()=>
 {
-    index++;
     if(index > pokemon_data.length-1)
     {
         index = 0;
     }
-       const div = document.createElement('div')
-       const h4 =  document.createElement('h4')
-       div.classList.add('card'); 
-       image_value(pokemon_data[index].url,div)
-    //    const para = document.createElement('p')
-       h4.textContent = pokemon_data[index].name
-       
-       div.append(h4)
-       info.append(div)
+    else{
+       addDiv(index);
+       index++;
+       addDiv(index);
+       index++;
+       addDiv(index);
+       index++;
+    }
 })
 
-// function spliceChildNodes(parent, start, deleteCount /*[, newNode1, newNode2]*/) {
-// 	var childNodes = parent.childNodes;
-// 	var removedNodes = [];
+function spliceChildNodes(parent, start, deleteCount /*[, newNode1, newNode2]*/) {
+	var childNodes = parent.childNodes;
+	var removedNodes = [];
 
-// 	// If `start` is negative, begin that many nodes from the end
-// 	start = start < 0 ? childNodes.length + start : start
+	// If `start` is negative, begin that many nodes from the end
+	start = start < 0 ? childNodes.length + start : start
 
-// 	// remove the element at index `start` `deleteCount` times
-// 	var stop = typeof deleteCount === 'number' ? start + deleteCount : childNodes.length;
-// 	for (var i = start; i < stop && childNodes[start]; i++) {
-// 		removedNodes.push(parent.removeChild(childNodes[start]));
-// 	}
+	// remove the element at index `start` `deleteCount` times
+	var stop = typeof deleteCount === 'number' ? start + deleteCount : childNodes.length;
+	for (var i = start; i < stop && childNodes[start]; i++) {
+		removedNodes.push(parent.removeChild(childNodes[start]));
+	}
 
-// 	// add new nodes at index `start`
-// 	if (arguments.length > 3) {
-// 		var newNodes = [].slice.call(arguments, 3);
+	// add new nodes at index `start`
+	if (arguments.length > 3) {
+		var newNodes = [].slice.call(arguments, 3);
 
-// 		// stick nodes in a document fragment
-// 		var docFrag = document.createDocumentFragment();
-// 		newNodes.forEach(function(el) {
-// 			docFrag.appendChild(el);
-// 		});
+		// stick nodes in a document fragment
+		var docFrag = document.createDocumentFragment();
+		newNodes.forEach(function(el) {
+			docFrag.appendChild(el);
+		});
 
-// 		// place in `parent` at index `start`
-// 		parent.insertBefore(docFrag, childNodes[start]);
-// 	}
+		// place in `parent` at index `start`
+		parent.insertBefore(docFrag, childNodes[start]);
+	}
 
-// 	return removedNodes;
-// }
+	return removedNodes;
+}
 
 
-callingApi();
+//setInterval(() => {
+  
+ 
+setInterval(()=>{
+   btn_next.click();
+   setTimeout(()=>{
+   spliceChildNodes(info,1,3)
+   },3000)
+},3000)
 
-setInterval(() => {
-// setTimeout(()=>{
-// btn_next.click();
-// },1000)    
-    
-    // interval.offset += 1;
-    
-  // if(info.hasChildNodes()) {
-  //   const val = info.children;
-  //   spliceChildNodes(info,0,2)
-  // }
-}, 500);
+// }, 1000);
+
 
